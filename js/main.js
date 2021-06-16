316a87c83c322802ecc365bb652ff6f60644aa3c
Andres Bedoya Tobon <andres.bedoya@upb.edu.co>
2021-06-16 17:57:07 -0500

    Actualizacion boton editar, interfaz

--- js/main.js
+++ js/main.js
@@ -1,159 +1,110 @@
-window.onload = inicio;
+var puesto_actual;
+var id_actual;
+var id_boton;
 
-/*window.onload = function(){
-	//var texto = "\"hola mundo\""; //"Let's Go!";//'"hola mundo"';
-	var texto = "Mensaje\n de\n varias\n lineas";
-	alert(texto);
-}*/
+window.onload = init;
 
-function inicio()
+function init()
 {
-	var mensaje = "Escoja la operacion asi:\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Potencia\n6. Raiz\n ";
-	/*mensaje = mensaje + "\n1. Suma";
-	mensaje = mensaje + "\n2. Resta";
-	mensaje = mensaje + "\n3. Multiplicacion";
-	mensaje = mensaje + "\n4. Division";
-	mensaje = mensaje + "\n5. Potencia";
-	mensaje = mensaje + "\n6. Raiz\n ";*/
-	
-	var operacion = prompt(mensaje);
-	var a, b;//Declaracion
-	// TODO LO QUE SE INGRESA CON EL TECLADO ES DE TIPO CADENA
+	//pintarCuadricula();
+	cerrar.addEventListener("click",cerrarVentana);
+	cargarReserva();
+}
 
-	operacion = Number(operacion);//NaN
-
-	if(operacion==1 || operacion==2 || operacion==3 || operacion==4 || operacion==5 || operacion==6)
+function cargarReserva(){
+	var puesto;
+	for(var i=1;i<=9;i++)
 	{
-		a = prompt("Ingrese a");
-		b = prompt("Ingrese b");
-		a = Number(a);
-		b = Number(b);
-		//Identacion del codigo, para darle orden
+		//console.log(localStorage.getItem("puesto_"+i));
+		if(localStorage.getItem("puesto_"+i)!=null)
+		{
+			puesto = document.getElementById("puesto_"+i);
+			puesto.className = "reservado";
+			puesto.innerHTML = localStorage.getItem("puesto_"+i);
+		}
+	}
+}
 
-		if(a != NaN && b!=NaN)
-		{
-			if(operacion == 1)
-			{
-				alert(a+b);
-			}
-			else
-			{
-				if(operacion == 2)
-				{	
-					alert(a-b);
-				}
-				else  
-				{
-					if(operacion == 3)
-					{	
-						alert(a*b);
-					}
-					else
-					{
-						if(operacion == 4)
-						{	
-							if(b != 0)
-							{
-								alert(a/b);
-							}
-							else
-							{
-								alert("Error: Division por cero");
-							}
-						}
-						else
-						{
-							if(operacion == 5)
-							{
-								alert("Potencia");
-							}
-							else
-							{
-								if(operacion == 6)
-								{
-									alert("Raiz");
-								}
-								else{
-									alert("Error: Operacion no valida");
-								}
-							}
-						}		
-					}	
-				}
-			}
-		}
-		else
-		{
-			alert("Error en el ingreso de datos")
-		}
+function cerrarVentana(){
+  ventana.className = "ligthbox hidden";
+}
+
+function crearReserva(numero){
+	id_actual = "puesto_"+numero;
+	id_boton = numero;
+	puesto_actual = document.getElementById(id_actual);
+	ventana.className = "ligthbox";
+	input_name.value = "";
+}
+
+function editarReserva(numero){
+	alert(numero);
+}
+
+function reservar(){
+	var temp;
+	if(input_name.value!="")
+	{
+		puesto_actual.className = "reservado";
+		temp = "<h2>Reservado</h2>"+input_name.value;
+		temp += '<img class="btn_editar" onClick="editarReserva('+id_boton+');" src="imgs/btn_editar.svg" alt="">';
+		puesto_actual.innerHTML = temp;
+		console.log(temp);
+		localStorage.setItem(id_actual,temp);
+		cerrarVentana();
 	}
 	else
 	{
-		alert("Error en la seleccion de la operacion");
+		alert("Error, introduzca el nombre de la reserva");
 	}
-	// document.writeln(typeof a);
-	// document.writeln(typeof b);
-
-	//NaN - Not a Number
-
-
-	/*
-
-	c = a + b;
-
-	document.write(c);*/
-
-	/*//Declaracion de variables
-	var a;
-	var b;
-	var c;
-
-	//Inicializacion
-	a = 5;
-	b = 2;
-
-	//Operacion y asignacion
-	c = a + b;
-
-	//Impresion en consola
-	console.log("suma",c);
-
-	//Asignacion o reasignacion o inicializacion
-	a = 24;
-	b = 2;
-
-	//Operacion y asignacion
-	c = a-b;
-
-	//Escritura en el documento, html concatenado con una variable que es c
-	document.write("<h2>"+c+"</h2>");
-
-	//Asignacion o reasignacion o inicializacion
-	a = 24;
-	b = 2;
-
-	//Operacion y asignacion
-	c = a*b;
-
-	//Impresion de alerta
-	alert("La multiplicacion es "+c);*/
-
-
-	// document.write("<h1 id='titulo'>Mensaje</h1>");
-	// document.write("<p>Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Rerum soluta perferendis corporis dolores, sit corrupti nam consequatur explicabo aut debitis nemo ullam harum a, eos impedit ut! Eos quod, ullam.</p>");
-	//document.getElementById("mensaje").innerHTML = "<marquee>Texto desde javascript</marquee>";
-	//document.write("<p>lorem</p>")
 }
-/*
-1. Crear dos variables
-2. Asignarles dos valores
-3. Imprimir, la suma, la resta y la multiplicacion
-4. Imprima usando 3 procedimientos diferentes
-*/
 
-/*1. sumar
-2. restar
-3. multiplicar
-4. dividir
-5. potencia
-6. raiz*/
+function pintarCuadricula(){
+	
+	var html = "";
+	//var inicio = 1;
+	var fin = 3;
+	var grid = document.getElementById("cuadricula");
+	var contador = 1;
+	var valor = 0;
+	var salon = [];
+	salon[0] = [true,false,true];
+	salon[1] = [false,false,true];
+	salon[2] = [true,false,false];
+
+	var fila = 0;
+	var col = 0;
+
+	console.log(salon[0][0]);
+	console.log(salon[1][1]);
+	console.log(salon[2][2]);
+
+	if(localStorage.getItem("esta_logeado")=="true")
+	{
+		while(contador<=fin)
+		{
+			console.log(fila,col);
+			valor = salon[0][col];
+			fila = fila + 1;
+			col = col + 1;
+
+			if(valor)// if(contador%2==0)
+			{
+				html = html + '<input type="button" class="naranja" value="'+contador+'">';
+			}
+			else
+			{
+				html = html + '<input type="button" class="gris" value="'+contador+'">';
+			}
+			contador = contador + 1;
+		}
+
+		grid.innerHTML = html;
+
+	}
+	else{
+		grid.innerHTML = "<h1>Esta seccion es bajo logeo</h1>";
+	}
+
+
+}
