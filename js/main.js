9fe0f856d0c06479e1ce5b219fd7f5d8289a4a09
Andres Bedoya Tobon <andres.bedoya@upb.edu.co>
2021-06-21 18:26:49 -0500

    Se agrega la logica para seleccionar la fecha y cargar los datos de reserva

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var puesto_actual;
 var id_actual;
 var id_boton;
 var reservas;
+var hora_actual;
+var horario;
 
 window.onload = init;
 
@@ -9,22 +11,34 @@ function init()
 {
 	//pintarCuadricula();
 	cerrar.addEventListener("click",cerrarVentana);
-	reservas = [];
+	hora_actual = localStorage.getItem("hora");
 	cargarReserva();
 }
 
 function cargarReserva(){
 	var puesto, usuario;
-	for(var i=1;i<=9;i++)//i = i + 1
+	reservas = [];
+
+	if(localStorage.getItem("horario")!=null)
 	{
-		//console.log(localStorage.getItem("puesto_"+i));
-		if(localStorage.getItem("puesto_"+i)!=null)
+		horario = JSON.parse(localStorage.getItem("horario"));
+		reservas = (horario[hora_actual]==null)?[]:horario[hora_actual];
+
+		if(reservas!=null)
 		{
-			puesto = document.getElementById("puesto_"+i);			
-			usuario = JSON.parse(localStorage.getItem("puesto_"+i));
-			actualizarEstado(puesto,usuario);
-			reservas[i] = usuario;
+			for(var i=1;i<=9;i++)//i = i + 1
+			{
+				if(reservas[i]!=null)
+				{
+					puesto = document.getElementById("puesto_"+i);			
+					usuario = reservas[i];
+					actualizarEstado(puesto,usuario);
+				}
+			}
 		}
+	}
+	else{
+		horario = [];
 	}
 }
 
@@ -65,61 +79,12 @@ function reservar(){
 		usuario = {nombre:input_name.value,id:id_boton};
 		actualizarEstado(puesto_actual,usuario);
 		reservas[id_boton] = usuario;
-		localStorage.setItem(id_actual,JSON.stringify(usuario));
+		horario[hora_actual] = reservas;
+		localStorage.setItem("horario",JSON.stringify(horario));
 		cerrarVentana();
 	}
 	else
 	{
 		alert("Error, introduzca el nombre de la reserva");
 	}
-}
-
-function pintarCuadricula(){
-	
-	var html = "";
-	//var inicio = 1;
-	var fin = 3;
-	var grid = document.getElementById("cuadricula");
-	var contador = 1;
-	var valor = 0;
-	var salon = [];
-	salon[0] = [true,false,true];
-	salon[1] = [false,false,true];
-	salon[2] = [true,false,false];
-
-	var fila = 0;
-	var col = 0;
-
-	console.log(salon[0][0]);
-	console.log(salon[1][1]);
-	console.log(salon[2][2]);
-
-	if(localStorage.getItem("esta_logeado")=="true")
-	{
-		while(contador<=fin)
-		{
-			console.log(fila,col);
-			valor = salon[0][col];
-			fila = fila + 1;
-			col = col + 1;
-
-			if(valor)// if(contador%2==0)
-			{
-				html = html + '<input type="button" class="naranja" value="'+contador+'">';
-			}
-			else
-			{
-				html = html + '<input type="button" class="gris" value="'+contador+'">';
-			}
-			contador = contador + 1;
-		}
-
-		grid.innerHTML = html;
-
-	}
-	else{
-		grid.innerHTML = "<h1>Esta seccion es bajo logeo</h1>";
-	}
-
-
 }
